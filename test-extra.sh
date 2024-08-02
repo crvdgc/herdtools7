e6bb9a80ee70c423366867b2d58898dfab4307ea
crvdgc <ubikium@gmail.com>
2024-08-02 18:19:51 +0100

    [test]: combine back tests

--- test-extra.sh
+++ test-extra.sh
@@ -5,11 +5,3 @@
 		-litmus-dir ./herd/tests/instructions/AArch64.extra \
 		"${@:-test}"
 
-./_build/default/internal/herd_regression_test.exe \
-		-j 8 \
-		-herd-path _build/install/default/bin/herd7 \
-		-libdir-path ./herd/libdir \
-		-litmus-dir ./herd/tests/instructions/AArch64.extra.sve \
-    -variant sve \
-		"${@:-test}"
-
